c522afc80d0be08a1dd28c9b7966114f81365fc2
trifonovtema <artyom85@gmail.com>
2024-08-26 15:54:53 +0300

    add register endpoint

--- backend/base_api/v1/router_auth.py
+++ backend/base_api/v1/router_auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from backend.core.schemas.user import UserRead, UserCreate
 from backend.dependencies.authentication.backend import authentication_backend
 from backend.base_api.v1.fastapi_users_router import fastapi_users
 from backend.settings import get_settings
@@ -10,8 +11,17 @@ router = APIRouter(
     tags=["Auth"],
 )
 
-
+# /login and /logout
 router.include_router(
-    fastapi_users.get_auth_router(authentication_backend),
+    fastapi_users.get_auth_router(
+        authentication_backend,
+    ),
     prefix="/jwt",
+)
+# /register
+router.include_router(
+    fastapi_users.get_register_router(
+        UserRead,
+        UserCreate,
+    ),
 )
