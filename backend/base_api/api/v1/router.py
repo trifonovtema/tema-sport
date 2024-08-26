3147ef24e5542b62fd49c9e71768c6bd25096d56
trifonovtema <artyom85@gmail.com>
2024-08-26 16:44:13 +0300

    add base api and v1 routers

---
+++ backend/base_api/api/v1/router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from backend.base_api.api.v1.competitions.router import router as router_competition
+from .router_auth import router as router_auth
+from .router_users import router as router_users
+from backend.base_api.api.v1.router_websocket import router as router_websocket
+from backend.settings import get_settings
+
+settings = get_settings()
+router = APIRouter(prefix=settings.base_api.v1.prefix)
+# router.include_router(router_user)
+router.include_router(router_competition)
+router.include_router(router_websocket)
+router.include_router(router_auth)
+router.include_router(router_users)
