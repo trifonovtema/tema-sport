95e4d8a96a3e095b74c9da97d5d0f2e5fbb2be95
trifonovtema <artyom85@gmail.com>
2024-08-26 22:31:14 +0300

    add HTTPBearer dependencie for tokens

--- backend/base_api/api/v1/router.py
+++ backend/base_api/api/v1/router.py
@@ -1,12 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
+
 from backend.base_api.api.v1.competitions.router import router as router_competition
 from .router_auth import router as router_auth
 from .router_users import router as router_users
 from backend.base_api.api.v1.router_websocket import router as router_websocket
 from backend.settings import get_settings
 
+http_bearer = HTTPBearer(auto_error=False)
+
 settings = get_settings()
-router = APIRouter(prefix=settings.base_api.v1.prefix)
+router = APIRouter(
+    prefix=settings.base_api.v1.prefix,
+    dependencies=[Depends(http_bearer)],
+)
 # router.include_router(router_user)
 router.include_router(router_competition)
 router.include_router(router_websocket)
