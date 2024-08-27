04eaf18fef6264733dd957747e6b68391766090b
trifonovtema <artyom85@gmail.com>
2024-08-27 15:38:56 +0300

    add id mixin that is generate default value for uuid with gen_random_uuid and for int with sequence

--- backend/core/models/mixins/id_pk.py
+++ backend/core/models/mixins/id_pk.py
@@ -1,8 +1,18 @@
+import uuid
+
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy import text
 
 from backend.settings import get_settings
 
 
 class IdPkMixin:
-    id: Mapped[get_settings().db.id_type.id_type] = mapped_column(primary_key=True)
+    # id: Mapped[get_settings().db.id_type.id_type] = mapped_column(primary_key=True)
+    if get_settings().db.id_type.id_type == uuid.UUID:
+        id: Mapped[uuid.UUID] = mapped_column(
+            primary_key=True,
+            server_default=text("gen_random_uuid()"),
+        )
+    elif get_settings().db.id_type.id_type == int:
+        id: Mapped[int] = mapped_column(primary_key=True)
