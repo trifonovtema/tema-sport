91b48ef6749fbbc192ffaa16d3d0af74524a7f6a
trifonovtema <artyom85@gmail.com>
2024-09-06 18:42:05 +0300

    add fastapi websocket tip

--- backend/base_api/api/v1/router_websocket.py
+++ backend/base_api/api/v1/router_websocket.py
@@ -23,3 +23,28 @@ async def websocket_endpoint(websocket: WebSocket, client_id: str):
         await kafka_consumer_manager.consumer_websocket.unregister_websocket(
             client_id=client_id
         )
+
+
+# from fastapi import FastAPI
+# from starlette.websockets import WebSocket
+#
+# app = FastAPI()
+#
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket) -> None:
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await websocket.send_text(f"Message text was: {data}")
+# You can use the async for notation:
+#
+# from fastapi import FastAPI
+# from starlette.websockets import WebSocket
+#
+# app = FastAPI()
+#
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket) -> None:
+#     await websocket.accept()
+#     async for data in websocket.iter_text():
+#         await websocket.send_text(f"Message text was: {data}")
