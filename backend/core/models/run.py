fd0d0c93b7c309dea2f497c91a80f00b93aa1b33
trifonovtema <artyom85@gmail.com>
2024-09-08 00:23:18 +0300

    set columns nullable=true

--- backend/core/models/run.py
+++ backend/core/models/run.py
@@ -5,7 +5,7 @@ from .base import Base
 from .mixins.created_at import CreatedAtMixin
 from core.config import settings
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, TIMESTAMP
 
 
 class Run(Base):
@@ -14,9 +14,19 @@ class Run(Base):
         user_id: settings.db.id_type_class.get_id_type() | None
         race_id: settings.db.id_type_class.get_id_type() | None
         name: str | None
-    #     scheduled_time: datetime | None
+        scheduled_time: datetime | None
     else:
-        name: Mapped[str] = mapped_column(index=True, nullable=False)
-        user_id: Mapped[settings.db.id_type_class.get_id_type()]
-        race_id: Mapped[settings.db.id_type_class.get_id_type()]
-        # scheduled_time: Mapped[datetime]
+        name: Mapped[str] = mapped_column(
+            index=True,
+            nullable=False,
+        )
+        user_id: Mapped[settings.db.id_type_class.get_id_type()] = mapped_column(
+            nullable=True,
+        )
+        race_id: Mapped[settings.db.id_type_class.get_id_type()] = mapped_column(
+            nullable=True,
+        )
+        scheduled_time: Mapped[datetime] = mapped_column(
+            TIMESTAMP(timezone=True),
+            nullable=True,
+        )
