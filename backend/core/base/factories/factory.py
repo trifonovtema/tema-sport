b0c45badecb2c62c9fff33c36341a6e2f7a4d92c
trifonovtema <artyom85@gmail.com>
2024-09-08 00:23:38 +0300

    comment unused factory

--- backend/core/base/factories/factory.py
+++ backend/core/base/factories/factory.py
@@ -8,39 +8,52 @@ from core.base.types import (
     CreateSchemaType,
     UpdateSchemaType,
     ReadSchemaType,
+    FilterSchemaType,
 )
 
 
-class Factory:
-    @staticmethod
-    def create_repository(
-        model: Type[ModelType],
-        create_schema: Type[CreateSchemaType],
-        update_schema: Type[UpdateSchemaType],
-    ) -> Callable[
-        [AsyncSession], BaseRepository[ModelType, CreateSchemaType, UpdateSchemaType]
-    ]:
-        def get_repo(
-            session: AsyncSession,
-        ) -> BaseRepository[ModelType, CreateSchemaType, UpdateSchemaType]:
-            return BaseRepository(model, session)
-
-        return get_repo
-
-    @staticmethod
-    def create_manager(
-        read_schema: Type[ReadSchemaType],
-    ) -> Callable[[BaseRepository], BaseManager[ReadSchemaType, BaseRepository]]:
-        def get_manager(
-            repository: BaseRepository,
-        ) -> BaseManager[ReadSchemaType, BaseRepository]:
-            return BaseManager(repository)
-
-        return get_manager
-
-    @staticmethod
-    def create_service() -> Callable[[BaseManager], BaseService]:
-        def get_service(manager: BaseManager) -> BaseService:
-            return BaseService(manager)
-
-        return get_service
+# class Factory:
+#     @staticmethod
+#     def create_repository(
+#         model: Type[ModelType],
+#         create_schema: Type[CreateSchemaType],
+#         update_schema: Type[UpdateSchemaType],
+#     ) -> Callable[
+#         [AsyncSession],
+#         BaseRepository[ModelType, CreateSchemaType, UpdateSchemaType, FilterSchemaType],
+#     ]:
+#         def get_repo(
+#             session: AsyncSession,
+#         ) -> BaseRepository[
+#             ModelType,
+#             CreateSchemaType,
+#             UpdateSchemaType,
+#             FilterSchemaType,
+#             FilterSchemaType,
+#         ]:
+#             return BaseRepository(model, session)
+#
+#         return get_repo
+#
+#     @staticmethod
+#     def create_manager(
+#         read_schema: Type[ReadSchemaType],
+#     ) -> Callable[
+#         [BaseRepository], BaseManager[ReadSchemaType, BaseRepository, FilterSchemaType]
+#     ]:
+#         def get_manager(
+#             repository: BaseRepository,
+#         ) -> BaseManager[ReadSchemaType, BaseRepository, FilterSchemaType]:
+#             return BaseManager(repository)
+#
+#         return get_manager
+#
+#     @staticmethod
+#     def create_service() -> Callable[
+#         [BaseManager],
+#         BaseService,
+#     ]:
+#         def get_service(manager: BaseManager) -> BaseService:
+#             return BaseService(manager)
+#
+#         return get_service
