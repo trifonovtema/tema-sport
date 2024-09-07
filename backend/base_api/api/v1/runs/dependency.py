4cc4ea2e6e5533223dd8b78cd3580b1c9106c951
trifonovtema <artyom85@gmail.com>
2024-09-07 17:03:07 +0300

    add run dependecies

---
+++ backend/base_api/api/v1/runs/dependency.py
@@ -0,0 +1,57 @@
+from fastapi import Depends
+from core.base.base_manager import BaseManager
+from core.base.base_repo import BaseRepository
+from core.base.base_service import BaseService
+from core.base.factories.factory import Factory
+from core.models import db_helper, Run
+from typing import AsyncGenerator, Annotated
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_sessionmaker,
+    AsyncSession,
+)
+from core.schemas.run import CreateRun, UpdateRun, ReadRun
+
+
+class RunRepository(BaseRepository[Run, CreateRun, UpdateRun]):
+    pass
+
+
+class RunManager(BaseManager[ReadRun, RunRepository]):
+    pass
+
+
+class RunService(BaseService[ReadRun, RunManager]):
+    pass
+
+
+# async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+#     async with db_helper.session_getter() as session:
+#         yield session
+
+
+async def get_async_session() -> AsyncSession:
+    async with db_helper.session_factory() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+
+
+async def get_run_repo(
+    session: Annotated[AsyncSession, Depends(get_async_session)],
+) -> RunRepository:
+    return RunRepository(Run, session)
+
+
+async def get_run_manager(
+    repo: Annotated[RunRepository, Depends(get_run_repo)],
+) -> RunManager:
+    return RunManager(repo, ReadRun)
+
+
+async def get_run_service(
+    manager: Annotated[RunManager, Depends(get_run_manager)],
+) -> RunService:
+    return RunService(manager)
