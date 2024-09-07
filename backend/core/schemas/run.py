a28831ca9be3779cc9ef31864edfc2608fdb6262
trifonovtema <artyom85@gmail.com>
2024-09-08 00:19:30 +0300

    set UpdateRun and FilterRun schemas

--- backend/core/schemas/run.py
+++ backend/core/schemas/run.py
@@ -12,18 +12,28 @@ class BaseRun(BaseModel):
     user_id: settings.db.id_type_class.get_id_type() | None
     race_id: settings.db.id_type_class.get_id_type() | None
     name: str | None
-    # scheduled_time: datetime | None
+    scheduled_time: datetime | None
 
 
 class ReadRun(BaseRun):
     id: settings.db.id_type_class.get_id_type()
-    # created_at: datetime | None
+    created_at: datetime | None
     model_config = ConfigDict(from_attributes=True)
 
 
 class UpdateRun(BaseRun):
-    pass
+    user_id: settings.db.id_type_class.get_id_type() | None = None
+    race_id: settings.db.id_type_class.get_id_type() | None = None
+    name: str | None = None
+    scheduled_time: datetime | None = None
 
 
 class CreateRun(BaseRun):
     pass
+
+
+class FilterRun(BaseModel):
+    user_id: settings.db.id_type_class.get_id_type() | None = None
+    race_id: settings.db.id_type_class.get_id_type() | None = None
+    name: str | None = None
+    scheduled_time: datetime | None = None
